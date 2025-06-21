3b14fd693f6f1ceb9bc081d06b7cd33fa50aa1bb
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-06-21 23:02:06 +0700

    .

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,13 @@
 import Refactor from "../Refactor.js";
 import fs from "fs";
 
-let analize = Refactor.analyzeFile("./test/test.js");
-let analizes = Refactor.analyzeFiles("./test/", "./result/");
+let analize = Refactor.analyzeFile(
+  "D:/code/my code/split-file-javascript/demo/test/test.js"
+);
+let analizes = Refactor.analyzeFiles(
+  "D:/code/my code/split-file-javascript/demo/test/",
+  "./result/"
+);
 
 const code = fs.readFileSync("sample-split-config.json", "utf8");
 let extractConfig = JSON.parse(code);
